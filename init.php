55b666557a07384eba5a0e55a2d857c3c4b0c2b7
Paul Rojas <paulrojas@iMac-de-Paul.local>
2015-09-19 12:25:45 -0500

    PJRR Second commit

--- init.php
+++ init.php
@@ -13,3 +13,11 @@ it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 */
+
+<?php
+function fancybox_wp_setup(){
+    wp_enqueue_style("jquery.customautocomplete", WP_PLUGIN_URL."/css/aristo/jquery-ui-aristo.min.css", false, "");
+    wp_enqueue_script("jquery");
+    wp_enqueue_script("jquery.ui", WP_PLUGIN_URL."/js/jquery-ui-1.9.2.custom.min.js", array("jquery"), "", 1);
+    wp_enqueue_script("jquery.customautocomplete", WP_PLUGIN_URL."/js/jquery.customautocomplete.js", array("jquery", "jquery.ui"), "", 1);
+}
---
+++ js/jquery.customautocomplete.js
@@ -0,0 +1,36 @@
+$(function() {
+    $( "#location" ).autocomplete({
+        source: function( request, response ) {
+            $.ajax({
+                url: "./api/getLocation.php",
+                dataType: "json",
+                data: {
+                    q: request.term
+                },
+                success: function( data ) {
+                    response( data );
+                }
+            });
+        },
+        minLength: 3,
+        focus: function( event, ui ) {
+            $( this ).val( ui.item.location );
+            return false;
+        },
+        select: function( event, ui ) {
+            location = "#" + ui.item.slug;
+            return false;
+        },
+        open: function() {
+            $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+        },
+        close: function() {
+            $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+        }
+    })
+    .autocomplete( "instance" )._renderItem = function( ul, item ) {
+        return $( "<li>" )
+            .append( "<a>" + item.location + "<br>Population:" + item.population + "</a>" )
+            .appendTo( ul );
+    };
+});

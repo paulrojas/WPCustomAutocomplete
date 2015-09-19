72796ae128ae4167875dd80a74105d4e5832d1ea
Paul Rojas <paulrojas@iMac-de-Paul.local>
2015-09-19 12:05:20 -0500

    PJRR First commit

---
+++ init.php
@@ -0,0 +1,15 @@
+/*
+Plugin Name: WP Custom Autocomplete
+Plugin URI: https://github.com/paulrojas/WPCustomAutocomplete
+Description: PHP test for WalletHub
+Version: 1.0
+Author: Paul Rojas
+Author URI: http://www.paulrojas.me
+License: Creative Commons Attribution-ShareAlike
+
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+*/
---
+++ readme.txt
@@ -0,0 +1,1 @@
+This is a plugin created to met the evaluation criteria at WalletHub
